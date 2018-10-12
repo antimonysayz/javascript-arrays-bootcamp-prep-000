35f9226b0eb5320d0d8f99e20795b8850e960fb5
Anthony Mai <adonksmasher@gmail.com>
2018-10-12 00:33:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array 
 }
 
-function removeELementFromEndOfARray(array) {
+function removeElementFromEndOfARray(array) {
   array.slice(0,array.length-1)
   return array
 }
